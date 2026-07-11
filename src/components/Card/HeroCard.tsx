895a2feda4febb4a7c776e4f44a242b2fead63c5
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-07-11 17:49:00 +0430

    Changes added

---
+++ src/components/Card/HeroCard.tsx
@@ -0,0 +1,10 @@
+export default function HeroCard() {
+  return (
+    <div className="w-full relative">
+      <div></div>
+      <div>
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Hero.tsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+  return (
+    <div className="w-full max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div></div>
+    </div>
+  );
+}
