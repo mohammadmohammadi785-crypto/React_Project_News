43c89b2d544623cd3fd8f2773bfeac28ba419a8c
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-07-12 14:15:16 +0430

    Changes added

--- src/components/MainSection/WhatsNew.tsx
+++ src/components/MainSection/WhatsNew.tsx
@@ -4,7 +4,7 @@ export default function WhatsNew() {
   return (
     <div className="w-full">
       <div className="flex justify-center items-center gap-20 w-full">
-        <h1 className="text-blue-900 text-xl font-medium">Whats' New</h1>
+        <h1 className="text-blue-900 text-xl font-medium">What's New</h1>
         <div className="flex text-xs gap-5">
           <ChevronLeft className="text-light-blue" />
           <ChevronRight className="text-light-blue" />
