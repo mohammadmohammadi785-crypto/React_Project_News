1557ae707af8b58fc60d21175cb353dac81cfa24
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-07-22 22:08:46 +0430

    Changes added

--- src/api/getLatestNews.ts
+++ src/api/getLatestNews.ts
@@ -4,7 +4,8 @@ const ai = new GoogleGenAI({});
 
 export async function GetLatestNews() {
   const data = await ai.interactions.create({
-    model: "gemini-3.5-flash",
+    generation_config: {},
+    model: "gemini-2.5-flash",
     input:
       "Give me the latest news about afghanistan witch contains one image, title, body and date in json format",
   });
